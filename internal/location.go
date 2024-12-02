f651006898b27532f1d4d7e294906bedbc76f669
Dan Clough <dan@clough.xyz>
2024-12-02 08:47:32 -0600

    Skip timestamp update for failed cron runs
    Changed RunCron() to only update timestamp when the backup succeeds

--- internal/location.go
+++ internal/location.go
@@ -449,11 +449,11 @@ func (l Location) RunCron() error {
 	next := schedule.Next(last)
 	now := time.Now()
 	if now.After(next) {
-		lock.SetCron(l.name, now.Unix())
 		errs := l.Backup(true, false, "")
 		if len(errs) > 0 {
 			return fmt.Errorf("Failed to backup location \"%s\":\n%w", l.name, errors.Join(errs...))
 		}
+		lock.SetCron(l.name, now.Unix())
 	} else {
 		if !flags.CRON_LEAN {
 			colors.Body.Printf("Skipping \"%s\", not due yet.\n", l.name)
